4c224d328aae1a1c671c00c999143a92106c7763
Anton Schirg <anton.schirg@posteo.de>
2020-08-29 23:38:05 +0200

    generate full site and stuff

--- generate.py
+++ generate.py
@@ -8,11 +8,15 @@ import argparse
 import click
 import jinja2
 from jinja2 import Environment, FileSystemLoader
+import http
 import http.server
 import socketserver
 import tempfile
 from PIL import Image
 from resizeimage import resizeimage
+import functools
+import io
+import traceback
 
 root_path = os.path.join(sys.path[0], '..')
 sys.path.insert(1, root_path)
@@ -61,19 +65,28 @@ class Structure(object):
                 return None
         return page
 
-    def getPageByPath(self, path_arr):
-        page = self.root_page
+    def getPageByPath(self, path_arr, relative_to=None):
+        page = relative_to if relative_to is not None else self.root_page 
         while len(path_arr) > 0:
             path_part = path_arr[0]
             path_arr = path_arr[1:]
-            foundPage = False
-            for i in page.children:
-                if i.pathPart() == path_part:
-                    page = i
-                    foundPage = True
-                    break
-            if not foundPage:
-                return None
+
+            if path_part == ".":
+                continue
+
+            if path_part == "..":
+                page = page.parent
+                if page is None:
+                    return None
+            else:
+                foundPage = False
+                for i in page.children:
+                    if i.pathPart() == path_part:
+                        page = i
+                        foundPage = True
+                        break
+                if not foundPage:
+                    return None
         return page
 
     def printMenu(self, lang, filed, current_page=None):
@@ -182,29 +195,6 @@ class Page(object):
             text = self.menuTitle(lang)
         print('<a href="' + self.url(lang) + '">' + text + '</a>', file=filed)
 
-    def printContent(self, lang, filed):
-        template_regex = re.compile(r'{{([^}]*)}}')
-        with open(os.path.join(self.path, lang+'.html'), 'r', encoding='utf-8') as content_file:
-            for line in content_file.readlines():
-                line_matches = template_regex.finditer(line)
-                last_end = 0
-                for line_match in line_matches:
-                    values = line_match.groups()[0].split("|")
-                    key = values[0]
-                    params = values[1:]
-                    filed.write(line[last_end:line_match.start()])
-                    if key == 'href':
-                        href_page = structure.getPageByPath(params)
-                        if href_page is None:
-                            print("In page ", self.path, " - language: ", lang, file=sys.stderr)
-                            print("Could not find page with path: ", params, file=sys.stderr)
-                        else:
-                            filed.write(href_page.url(lang))
-                    else:
-                        filed.write('Undefined')
-                    last_end = line_match.end()
-                filed.write(line[last_end:len(line)])
-
     def printLanguages(self, filed):
         global languages
         print("<ul>", file=filed)
@@ -216,66 +206,109 @@ class Page(object):
         print("</ul>", file=filed)
 
 
-def generatePage(structure, page, lang, root_dir, url = None):
-    filename = root_dir + (page.url(lang) if url is None else url)
-    dirname = os.path.dirname(filename)
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    template_regex = re.compile(r'{{(.*)}}')
-
-    with open(filename , 'w', encoding='utf-8') as filed:
-        with open(os.path.join(root_path, 'template'), 'r', encoding='utf-8') as f:
-            for line in f.readlines():
-                line_match = template_regex.search(line)
-                if line_match is not None:
-                    key = line_match.groups()[0]
-                    filed.write(line[0:line_match.start()])
-                    if key == 'content':
-                        page.printContent(lang, filed)
-                    elif key == 'menu':
-                        structure.printMenu(lang, filed, page)
-                    elif key == 'languages':
-                        page.printLanguages(filed)
-                    elif key == 'title':
-                        print(page.title(lang), file=filed, end="")
-                    elif key == 'language-code':
-                        print(lang, file=filed, end="")
-                    else:
-                        filed.write('Undefined')
-                    filed.write(line[line_match.end():len(line)])
-                else:
-                    filed.write(line)
-
-
-
-from jinja2 import BaseLoader, TemplateNotFound
-
-class DirectLoader(BaseLoader):
-
-    def __init__(self):
-        pass
-
+class DirectLoader(jinja2.BaseLoader):
     def get_source(self, environment, template):
         path = template
         if not os.path.exists(path):
-            raise TemplateNotFound(template)
+            raise jinja2.TemplateNotFound(template)
         mtime = os.path.getmtime(path)
         with open(path, 'r', encoding='utf-8') as f:
             source = "{% extends 'base.html' %}{% block content %}" + f.read() + "{% endblock %}"
         return source, path, lambda: mtime == os.path.getmtime(path)
 
+structure = Structure()
+
+
+def generate_image(path, width, max_width, out_dir):
+    '''
+    Generates an image with the specified width and returns its path.
+    If max_width is specified and the width of the input image
+    is smaller than max_width the image is not modified.
+    '''
+    print(f"generate_image {path} {width}")
+    new_name = os.path.splitext(path)[0] + "-" + str(width) + os.path.splitext(path)[1]
+    with open(out_dir + path, 'r+b') as f:
+        with Image.open(f) as image:
+            if max_width is not None and image.width < max_width:
+                return path
+            img = resizeimage.resize_width(image, width, validate=False)
+            img.save(out_dir + new_name, image.format)
+    return new_name
+
+
+def generate_link(path, lang, page):
+    print(f"generate_link {path} (lang={lang}) from {page.url(lang)}")
+    params = path.split("/")
+    relative_to = None
+    if params[0] == ".":
+        relative_to = page
+    if params[0] == "..":
+        relative_to = page
+    if params[0] == "":  # path starts with /
+        params = params[1:]
+    href_page = structure.getPageByPath(params, relative_to)
+    if href_page is None:
+        raise RuntimeError(f"Link undefined - path {path} does not exist")
+        #return jinja2.Undefined(name="href")
+    else:
+        return href_page.url(lang)
+
+
+def check_exists(path, out_dir):
+    print(f"check_exists {path}")
+    if not os.path.exists(out_dir + path):
+        raise RuntimeError(f"Path {path} does not exist")
+    else:
+        return path
+
+
+def generate_page(page, lang, structure, out_dir, url=None):
+    template_path = os.path.join(page.path, lang+'.html')
+    page_path = page.url(lang) if url is None else url
+
+    filename = out_dir + page_path
+
+    print(f"generate_page {template_path} to {filename}")
+
+    env = Environment(loader=FileSystemLoader(['templates']))
+    loader = DirectLoader()
+
+    env.globals = {
+        "href": functools.partial(generate_link, lang=lang, page=page),
+        "generate_image": functools.partial(generate_image, out_dir=out_dir),
+        "check_exists": functools.partial(check_exists, out_dir=out_dir)
+    }
+    template = loader.load(env, template_path, env.globals)
+
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    menu = io.StringIO()
+    structure.printMenu(lang, menu, page)
+
+    languages = io.StringIO()
+    page.printLanguages(languages)
+
+    args = {
+        "title": page.title(lang),
+        "language_code": lang,
+        "menu": menu.getvalue(),
+        "languages": languages.getvalue()
+    }
+
+    with open(filename, "w") as fh:
+        fh.write(template.render(**args))
+
 
 @click.command()
 @click.option("--port", default=5000)
 def serve(port):
-    env = Environment(loader=FileSystemLoader(['templates']))
-    loader = DirectLoader()
-
-    structure = Structure()
 
     with tempfile.TemporaryDirectory() as out_dir:
         print(f"Serving pages from {out_dir}")
 
+        # Copy static content to out_dir
         src = 'content/static'
         src_files = os.listdir(src)
         for file_name in src_files:
@@ -286,57 +319,6 @@ def serve(port):
             else:
                 shutil.copytree(full_file_name, full_dest_name)
 
-        def generate_image(path, width, max_width=None):
-            '''
-            Generates an image with the specified width and returns its path.
-            If max_width is specified and the width of the input image
-            is smaller than max_width the image is not modified.
-            '''
-            print(f"generate_image {path} {width}")
-            new_name = os.path.splitext(path)[0] + "-" + str(width) + os.path.splitext(path)[1]
-            with open(out_dir + path, 'r+b') as f:
-                with Image.open(f) as image:
-                    if max_width is not None and image.width < max_width:
-                        return path
-                    img = resizeimage.resize_width(image, width, validate=False)
-                    img.save(out_dir + new_name, image.format)
-            return new_name
-
-        env.globals['generate_image'] = generate_image
-
-        def generate_link(path, lang):
-            print(f"generate_link {path}")
-            params = path.split("/")
-            href_page = structure.getPageByPath(params)
-            if href_page is None:
-                return jinja2.Undefined(name="href")
-            else:
-                return href_page.url(lang)
-
-        env.globals['generate_link'] = generate_link
-
-
-        def generate_page(page, lang):
-            template_path = os.path.join(page.path, lang+'.html')
-            page_path = page.url(lang)
-
-            filename = out_dir + page_path
-
-            print(f"generate_page {template_path} to {filename}")
-
-            template = loader.load(env, template_path, env.globals)
-
-            dirname = os.path.dirname(filename)
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
-            args = {
-                "title": "Testpage",
-                "language_code": lang,
-                "href": (lambda path: generate_link(path, lang))
-            }
-
-            with open(filename, "w") as fh:
-                fh.write(template.render(**args))
 
         class Handler(http.server.SimpleHTTPRequestHandler):
             '''
@@ -358,8 +340,13 @@ def serve(port):
 
                     page = structure.getPageByUrl(url, lang)
                     if page is not None:
-                        #TODO Handle errors and send 500
-                        generate_page(page, lang)
+                        try:
+                            #TODO Handle errors and send 500
+                            generate_page(page, lang, structure, out_dir)
+                        except Exception as e:
+                            self.send_error(http.HTTPStatus.INTERNAL_SERVER_ERROR, "Failed to generate page")
+                            print(traceback.format_exc())
+                            return None
                 return super().send_head()
 
 
@@ -369,10 +356,11 @@ def serve(port):
             httpd.serve_forever()
 
 @click.command()
-@click.option("--out_dir", prompt="Generate in folder")
+@click.option("--out_dir")
 def generate(out_dir):
-    return
-    # TODO adapt to flask
+    if out_dir.endswith("/"):
+        out_dir = out_dir[0:-1]
+
     structure = Structure()
 
     #Delete existing output
@@ -387,17 +375,6 @@ def generate(out_dir):
             except Exception as e:
                 print(e)
 
-    #Generate pages
-    def recurse_pages(page):
-        for lang, langname in languages:
-            generatePage(structure, page, lang, out_dir)
-        for c in page.children:
-            recurse_pages(c)
-
-    recurse_pages(structure.root_page)
-    #Index with default language
-    generatePage(structure, structure.root_page, config.default_language, out_dir, url='/index.html')
-
     #Copy static content
     src = os.path.join(root_path, 'content/static')
     src_files = os.listdir(src)
@@ -408,6 +385,17 @@ def generate(out_dir):
             shutil.copy(full_file_name, out_dir)
         else:
             shutil.copytree(full_file_name, full_dest_name)
+
+    #Generate pages
+    def recurse_pages(page):
+        for lang, langname in languages:
+            generate_page(page, lang, structure, out_dir)
+        for c in page.children:
+            recurse_pages(c)
+
+    recurse_pages(structure.root_page)
+    #Index with default language
+    generate_page(structure.root_page, config.default_language, structure, out_dir, url='/index.html')
 
 @click.group()
 def cli():
